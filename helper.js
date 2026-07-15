54d2ec8fef98ac8f4f50588f29c66ec8c180eceb
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2026-07-14 20:45:33 -0400

    Refactor input validation logic and enhance error handling for caching proxy commands

--- helper.js
+++ helper.js
@@ -4,7 +4,7 @@ const inputRules = new Map([
     {
       type: "number",
       isRequired: true,
-      nonNegativeValue: true
+      nonNegativeValue: true,
     },
   ],
   [
@@ -16,18 +16,56 @@ const inputRules = new Map([
   ],
 ]);
 
-const inputValidation = (input, value) => {
-    console.log(`input: ${input}, value: ${value}`)
-}
+const getInputById = (id) => inputRules.get(id);
+
+const isAProperty = (property) => property?.startsWith("--");
+
+const inputValidation = (id, value) => {
+  const input = getInputById(id);
+
+  if (!input) {
+    console.error(
+      `The id ${id} is not a valid property, the properties valid are ${[...inputRules.keys()].join(", ")}.`,
+    );
+    return true;
+  }
+
+  if (input.isRequired && (!value || isAProperty(value))) {
+    console.error(`The id ${id} must have a value.`);
+    return true;
+  }
+
+  if (input.type == "number") {
+    const inputValue = Number(value);
+    if (input.nonNegativeValue && inputValue < 0) {
+      console.error(`The id ${id} must be a positive number.`);
+      return true;
+    }
+  }
+
+  return false;
+};
 
 export const inputValidations = (arg) => {
-    if(arg.length != 4) {
-        console.log("Please you must specify all the properties and values, for example: --port 3000 --origin http://dummyjson.com")
-        return false
-    }
+  const properties = arg?.filter((arg) => arg.startsWith("--"));
+  const hasAllRequiredProperties = inputRules?.size == properties.length;
 
-    const propertiesCount = arg.length / 2
-    for(let i = 0; i < propertiesCount; i++) {
-        inputValidation((i == 0 ? arg[i] : arg[i + 1]), (i == 0 ? arg[i+1] : arg[i + 2]))
-    }
-}
+  if (!arg.length || !hasAllRequiredProperties) {
+    console.error(
+      "Please you must specify all the properties and values.",
+    );
+    return false;
+  }
+
+  let hasError = false;
+
+  for (let i = 0; i < properties.length; i++) {
+    hasError = inputValidation(
+      i == 0 ? arg[i] : arg[i + 1],
+      i == 0 ? arg[i + 1] : arg[i + 2],
+    );
+    if (hasError) break;
+  }
+
+  return hasError;
+};
