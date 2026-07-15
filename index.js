cbdc7dc2a7e0dc4e1e75dc2323f1d4040b4144f7
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2026-07-14 20:14:36 -0400

    Implement command handling for caching proxy and exit commands

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+process.stdin.on("data", (data) => {
+    const input = data.toString().trim().split(" ")
+    const command = input[0]
+
+    const arg = input.slice(1)
+
+    switch(command) {
+        case "caching-proxy":
+            console.log("Caching proxy: ")
+            break
+        case "exit":
+            process.exit()
+        default:
+            console.log("Command not valid.")
+    }
+})
